45c8330960e99a736d3390a5502cf6ab9d547b5b
Slain505 <vladyslav.yefimenko@protonmail.com>
2022-12-14 23:25:10 +0100

    no message

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,9 +1,13 @@
+using System.Runtime.Serialization;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] float steerSpeed = 0.5f;
+    [SerializeField] float moveSpeed = 0.1f;
+    
     void Start()
     {
         
